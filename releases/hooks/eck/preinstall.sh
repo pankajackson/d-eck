699cb732b27eda40d9fc4306411ed68c8f6198ef
Pankaj Jackson <pankajackson@live.co.uk>
2023-05-11 14:02:25 +0530

    Cleanup

--- releases/hooks/eck/preinstall.sh
+++ releases/hooks/eck/preinstall.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 set -u
 
-helm upgrade elastic-operator elastic/eck-operator -n elastic-system --create-namespace --version 2.6.1
+OPERATOR_REPO='elastic/eck-operator'
+OPERATOR_VERSION='2.6.1'
+OPERATOR_NS='elastic-system'
+
+helm install elastic-operator $OPERATOR_REPO -n $OPERATOR_NS --create-namespace --version $OPERATOR_VERSION ||\
+helm upgrade elastic-operator $OPERATOR_REPO -n $OPERATOR_NS --create-namespace --version $OPERATOR_VERSION
